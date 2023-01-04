247e365f39de8f8a901cfd919c66f6d24cc6fd49
faranak.kheirandish@gmail.com <faranak.kheirandish@gmail.com>
2023-01-04 23:01:49 +0000

    create stage payment file

--- models/staging/jaffle_shop/stg_payments.sql
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -1,12 +1,9 @@
-with source as
-(
-    select * from {{ source('stripe','payment')}}
-)
-select 
+with source as (select * from {{ source("stripe", "payment") }})
+select
     id as customer_id,
     orderid as order_id,
     status,
     paymentmethod as payment_method,
-    amount/100 as amount,
+    amount / 100 as amount,
     created as created_at
 from source
