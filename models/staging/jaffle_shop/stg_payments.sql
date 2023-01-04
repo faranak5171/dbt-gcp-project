ffd9cb86cf0654dcaaacf09f6d8c84bdaee1b476
faranak.kheirandish@gmail.com <faranak.kheirandish@gmail.com>
2023-01-04 23:01:07 +0000

    create stage order

---
+++ models/staging/jaffle_shop/stg_payments.sql
@@ -0,0 +1,12 @@
+with source as
+(
+    select * from {{ source('stripe','payment')}}
+)
+select 
+    id as customer_id,
+    orderid as order_id,
+    status,
+    paymentmethod as payment_method,
+    amount/100 as amount,
+    created as created_at
+from source
